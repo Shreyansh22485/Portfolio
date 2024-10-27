0872a4f0c2ab7eb9b7c6a38bf3b5ee257ca78c74
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2024-10-28 00:53:13 +0530

    final changes

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -11,7 +11,7 @@ export default function Contact() {
     <>
       <Image
         src={bg}
-        alt="Next.js Portfolio website's contact page background image"
+        alt="Shreyansh Srivastav"
         priority
         sizes="100vw"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
